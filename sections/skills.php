6596d4ac61ad439d554b8d97ff44d020291fd4e7
Atanas Atanasov <scriptex.bg@gmail.com>
2018-03-11 22:20:38 +0200

    Update skills section text

--- sections/skills.php
+++ sections/skills.php
@@ -1,3 +1,3 @@
 <section class="c-section c-section--skills" id="skills">
-	<h2>Skills</h2>
+	<h2>Skills <br><small>(Drag the bubbles to play)</small></h2>
 </section><!-- /#skills.c-section c-section-/-skills -->
