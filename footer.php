97afa40052f80411148527a7326013dc57862042
Atanas Atanasov <scriptex.bg@gmail.com>
2018-03-11 22:40:40 +0200

    Recode the footer

--- footer.php
+++ footer.php
@@ -1,56 +1,45 @@
+<?php $links = [
+	[
+		'url'  => 'mailto:hi@atanas.info',
+		'icon' => 'email'
+	],
+	[
+		'url'  => 'https://www.linkedin.com/in/scriptex/',
+		'icon' => 'linkedin'
+	],
+	[
+		'url'  => 'https://github.com/scriptex',
+		'icon' => 'github'
+	],
+	[
+		'url'  => 'https://gitlab.com/scriptex',
+		'icon' => 'gitlab'
+	],
+	[
+		'url'  => 'https://twitter.com/scriptexbg',
+		'icon' => 'twitter'
+	],
+	[
+		'url'  => 'https://www.npmjs.com/~scriptex',
+		'icon' => 'npm'
+	],
+]; ?>
+
 <div class="c-footer">
 	<div class="o-shell o-shell--flex">
 		<p>&copy; <?php echo date( 'Y' ); ?> Atanas Atanasov. All rights reserved.</p>
 
 		<nav class="c-socials">
 			<ul>
-				<li>
-					<a href="mailto:hi@atanas.info" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-email">
-							<use xlink:href="#svg-email" />
-						</svg><!-- /.svg-email -->
-					</a>
-				</li>
-
-				<li>
-					<a href="https://www.linkedin.com/in/scriptex/" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-linkedin">
-							<use xlink:href="#svg-linkedin" />
-						</svg><!-- /.svg-linkedin -->
-					</a>
-				</li>
-
-				<li>
-					<a href="https://github.com/scriptex" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-github">
-							<use xlink:href="#svg-github" />
-						</svg><!-- /.svg-github -->
-					</a>
-				</li>
-
-				<li>
-					<a href="https://gitlab.com/scriptex" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-gitlab">
-							<use xlink:href="#svg-gitlab" />
-						</svg><!-- /.svg-gitlab -->
-					</a>
-				</li>
-
-				<li>
-					<a href="https://twitter.com/scriptexbg" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-twitter">
-							<use xlink:href="#svg-twitter" />
-						</svg><!-- /.svg-twitter -->
-					</a>
-				</li>
-
-				<li>
-					<a href="https://www.npmjs.com/~scriptex" target="_blank" rel="noopener noreferrer">
-						<svg class="svg-npm">
-							<use xlink:href="#svg-npm" />
-						</svg><!-- /.svg-npm -->
-					</a>
-				</li>
+				<?php foreach ($links as $link) : ?>
+					<li>
+						<a href="<?php echo $link['url'] ?>">
+							<svg class="svg-<?php echo $link['icon'] ?>">
+								<use xlink:href="#svg-<?php echo $link['icon'] ?>" />
+							</svg>
+						</a>
+					</li>
+				<?php endforeach ?>
 			</ul>
 		</nav><!-- /.c-socials -->
 	</div><!-- /.o-shell o-shell-/-flex -->
