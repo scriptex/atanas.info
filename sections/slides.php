4f23df89ce3dd4a5dab7039a3b705a4e7dd19e81
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-09 15:09:48 +0300

    Add slides section

---
+++ sections/slides.php
@@ -0,0 +1,31 @@
+<section class="c-section c-section--slides" id="slides">
+	<div class="o-shell">
+		<nav class="c-section__nav text-center">
+			<h2>Slides</h2>
+
+			<ul>
+				<li>
+					<label for="toggle-slides-1">Do more with Sublime Text</label>
+				</li>
+
+				<li>
+					<label for="toggle-slides-2">EcmaScript Features</label>
+				</li>
+			</ul>
+		</nav><!-- /.c-section__nav -->
+
+		<div class="c-section__body">
+			<input type="radio" name="toggle-slides" id="toggle-slides-1" class="hidden" checked />
+
+			<div class="c-section__frame">
+				<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTivcVmPyjYPdWgoJMChe_rrVyZEIw4ZgF37lCOlxUUm7DchxhMJouEV_BcrH6LDL8kiNmRSIWkiSxK/embed?start=false&loop=false&delayms=3000"></iframe>
+			</div><!-- /.c-section__frame -->
+
+			<input type="radio" name="toggle-slides" id="toggle-slides-2" class="hidden" />
+
+			<div class="c-section__frame">
+				<iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSA8qN0_3lb0YWDYJheJ3q0AAsXWlLLpP86pnmV0KWfPo_2G-SSdSK--GVRyJbwT8_kWyJ0TZxFoaH8/embed?start=false&loop=false&delayms=3000"></iframe>
+			</div><!-- /.c-section__frame -->
+		</div><!-- /.c-section__body -->
+	</div><!-- /.o-shell -->
+</section><!-- /#slides.c-section c-section--slides -->
