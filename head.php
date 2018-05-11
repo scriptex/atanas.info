419a6c842d094ee37087b311d4b8be3c31caad5a
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-11 08:59:30 +0300

    Update metadata

--- head.php
+++ head.php
@@ -1,7 +1,32 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>Atanas Atanasov | Full Stack Web Developer</title>
+<title>Atanas Atanasov | Senior Web Developer</title>
+
+<meta name="description" content="Atanas Atanasov - Senior Web Developer -== Custom WordPress themes, Single Page Applications -- Vue.js &amp; React, Laravel, State of the art Web Development ==-">
+<meta name="keywords" content="Web Development, WordPress Development, HTML Development, Website Development, JavaScript Development, WordPress, Laravel, WP, JS, JavaScript, Vue, Vue.js, VueJS, Nuxt, Single Page Apps, Single Page Application, CSS, CSS3, ES5, ES6, ES7, Website, Development">
+<meta name="copyright" content="Atanas Atanasov">
+<meta name="language" content="EN">
+<meta name="robots" content="index,follow">
+<meta name="author" content="Atanas Atanasov, hi@atanas.info">
+<meta name="designer" content="Atanas Atanasov">
+<meta name="coverage" content="Worldwide">
+<meta name="distribution" content="Worldwide">
+<meta name="rating" content="General">
+<meta name="revisit-after" content="7 days">
+<meta http-equiv="Expires" content="0">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+
+<meta name="application-name" content="Atanas Atanasov"/>
+<meta name="msapplication-TileColor" content="#ef4c23" />
+<meta name="msapplication-TileImage" content="assets/images/favicon/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content="assets/images/favicon/mstile-70x70.png" />
+<meta name="msapplication-square150x150logo" content="assets/images/favicon/mstile-150x150.png" />
+<meta name="msapplication-wide310x150logo" content="assets/images/favicon/mstile-310x150.png" />
+<meta name="msapplication-square310x310logo" content="assets/images/favicon/mstile-310x310.png" />
+
+<meta name="theme-color" content="#ef4c23" />
 
 <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicon/apple-touch-icon-57x57.png" />
 <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicon/apple-touch-icon-114x114.png" />
@@ -17,14 +42,6 @@
 <link rel="icon" type="image/png" href="assets/images/favicon/favicon-16x16.png" sizes="16x16" />
 <link rel="icon" type="image/png" href="assets/images/favicon/favicon-128.png" sizes="128x128" />
 <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon/favicon.ico" />
-<meta name="application-name" content="Atanas Atanasov"/>
-<meta name="msapplication-TileColor" content="#ef4c23" />
-<meta name="msapplication-TileImage" content="assets/images/favicon/mstile-144x144.png" />
-<meta name="msapplication-square70x70logo" content="assets/images/favicon/mstile-70x70.png" />
-<meta name="msapplication-square150x150logo" content="assets/images/favicon/mstile-150x150.png" />
-<meta name="msapplication-wide310x150logo" content="assets/images/favicon/mstile-310x150.png" />
-<meta name="msapplication-square310x310logo" content="assets/images/favicon/mstile-310x310.png" />
-<meta name="theme-color" content="#ef4c23" />
 
 <?php critical_css(); ?>
 
