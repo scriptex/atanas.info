d015db6504ccb6d94b80731fad5aa237d6ca5bd1
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-15 16:42:14 +0300

    Register the service worker

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -40,3 +40,21 @@ win.addEventListener('load', event => {
 win.addEventListener('scroll', event => {
 	toggleHeaderState(win.pageYOffset);
 });
+
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('./service-worker.js').then(
+			function(registration) {
+				// Registration was successful
+				console.log(
+					'ServiceWorker registration successful with scope: ',
+					registration.scope
+				);
+			},
+			function(err) {
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			}
+		);
+	});
+}
