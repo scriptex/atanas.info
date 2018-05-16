7e414ec6cc153a82c40a0aa2800db726df4d7f68
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-16 08:05:04 +0300

    Update service worker registration

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -42,19 +42,7 @@ win.addEventListener('scroll', event => {
 });
 
 if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function() {
-		navigator.serviceWorker.register('./service-worker.js').then(
-			function(registration) {
-				// Registration was successful
-				console.log(
-					'ServiceWorker registration successful with scope: ',
-					registration.scope
-				);
-			},
-			function(err) {
-				// registration failed :(
-				console.log('ServiceWorker registration failed: ', err);
-			}
-		);
+	win.addEventListener('load', () => {
+		navigator.serviceWorker.register('./service-worker.js');
 	});
 }
