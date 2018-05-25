629e1309c84f3208ae63adc0e7b89676d3c59912
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-25 07:56:37 +0300

    Update service worker's cache

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,15 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('atanas.info-cache').then(function(cache) {
 			return cache.addAll([
-				'/'
+				'/',
+				'/assets/dist/app.css',
+				'/assets/dist/app.js',
+				'/assets/dist/sprite.png',
+				'/assets/dist/sprite@2x.png',
+				'/assets/dist/sprite.svg',
+				'/assets/images/temp/atanas.jpg',
+				'/assets/images/temp/desktop.jpg',
+				'/assets/images/temp/logo.png'
 			]);
 		})
 	);
