a29306cf95e39fa2cd0406ef1fef055111b0ec60
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-30 08:45:21 +0300

    Update service worker

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,104 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'atanas.info-cache';
+const urlsToCache = [
+	'/index.php',
+	'/assets/dist/app.css',
+	'/assets/dist/app.js',
+	'/assets/dist/sprite.png',
+	'/assets/dist/sprite@2x.png',
+	'/assets/dist/sprite.svg',
+	'/assets/images/temp/atanas.jpg',
+	'/assets/images/temp/desktop.jpg',
+	'/assets/images/temp/logo.png',
+	'/assets/images/svg/angular.svg',
+	'/assets/images/svg/backbone.svg',
+	'/assets/images/svg/bash.svg',
+	'/assets/images/svg/bootstrap.svg',
+	'/assets/images/svg/codepen.svg',
+	'/assets/images/svg/cordova.svg',
+	'/assets/images/svg/css.svg',
+	'/assets/images/svg/d3.svg',
+	'/assets/images/svg/email.svg',
+	'/assets/images/svg/foundation.svg',
+	'/assets/images/svg/git.svg',
+	'/assets/images/svg/github.svg',
+	'/assets/images/svg/gitlab.svg',
+	'/assets/images/svg/google-plus.svg',
+	'/assets/images/svg/grunt.svg',
+	'/assets/images/svg/gulp.svg',
+	'/assets/images/svg/html.svg',
+	'/assets/images/svg/ionic.svg',
+	'/assets/images/svg/javascript.svg',
+	'/assets/images/svg/jquery.svg',
+	'/assets/images/svg/less.svg',
+	'/assets/images/svg/linkedin.svg',
+	'/assets/images/svg/logo.svg',
+	'/assets/images/svg/nodejs.svg',
+	'/assets/images/svg/npm.svg',
+	'/assets/images/svg/photoshop.svg',
+	'/assets/images/svg/php.svg',
+	'/assets/images/svg/postcss.svg',
+	'/assets/images/svg/react.svg',
+	'/assets/images/svg/sass.svg',
+	'/assets/images/svg/sketch.svg',
+	'/assets/images/svg/svg.svg',
+	'/assets/images/svg/svn.svg',
+	'/assets/images/svg/twitter.svg',
+	'/assets/images/svg/vue.svg',
+	'/assets/images/svg/webpack.svg',
+	'/assets/images/svg/wordpress.svg'
+];
+
+self.addEventListener('install', event => {
+	self.skipWaiting();
+
 	event.waitUntil(
-		caches.open('atanas.info-cache').then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/assets/dist/app.css',
-				'/assets/dist/app.js',
-				'/assets/dist/sprite.png',
-				'/assets/dist/sprite@2x.png',
-				'/assets/dist/sprite.svg',
-				'/assets/images/temp/atanas.jpg',
-				'/assets/images/temp/desktop.jpg',
-				'/assets/images/temp/logo.png'
-			]);
-		})
+		caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
 	);
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
 	event.respondWith(
-		caches.match(event.request).then(function(response) {
+		caches.match(event.request).then(response => {
 			if (response) {
 				return response;
 			}
 
-			return fetch(event.request);
+			const fetchRequest = event.request.clone();
+
+			return fetch(fetchRequest).then(response => {
+				if (
+					!response ||
+					response.status !== 200 ||
+					response.type !== 'basic'
+				) {
+					return response;
+				}
+
+				// Clone the response because it's a one time use stream
+				const responseToCache = response.clone();
+
+				event.waitUntil(
+					caches.open(CACHE_NAME).then(cache => {
+						cache.put(event.request, responseToCache);
+					})
+				);
+
+				return response;
+			});
 		})
+	);
+});
+
+self.addEventListener('activate', event => {
+	event.waitUntil(
+		caches
+			.keys()
+			.then(cacheNames =>
+				Promise.all(
+					cacheNames
+						.filter(cacheName => cacheName !== CACHE_NAME)
+						.map(cacheName => caches.delete(cacheName))
+				)
+			)
 	);
 });
