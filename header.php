2c601d3452296e5f4d00c28c8683691db7e310e7
Atanas Atanasov <scriptex.bg@gmail.com>
2018-05-30 08:20:45 +0300

    Add titles to links

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="c-header">
 	<div class="o-shell o-shell--flex">
-		<a href="#hello" class="c-logo js-internal-link">
+		<a href="#hello" class="c-logo js-internal-link" title="Back to top">
 			<svg class="svg-logo">
 				<use xlink:href="#svg-logo" />
 			</svg><!-- /.svg-logo -->
@@ -19,31 +19,31 @@
 		<nav class="c-nav">
 			<ul>
 				<li>
-					<a href="#about" class="js-internal-link">
+					<a href="#about" class="js-internal-link" title="About me">
 						<span>About</span>
 					</a>
 				</li>
 
 				<li>
-					<a href="#skills" class="js-internal-link">
+					<a href="#skills" class="js-internal-link" title="My skills">
 						<span>Skills</span>
 					</a>
 				</li>
 
 				<li>
-					<a href="projects/">
+					<a href="projects/" title="My open source projects">
 						<span>Open Source</span>
 					</a>
 				</li>
 
 				<li>
-					<a href="mailto:hi@atanas.info" target="_blank" rel="noopener noreferrer">
+					<a href="mailto:hi@atanas.info" target="_blank" rel="noopener noreferrer" title="Send me an email">
 						<span>Hire Me</span>
 					</a>
 				</li>
 
 				<li>
-					<a href="#slides" class="js-internal-link">
+					<a href="#slides" class="js-internal-link" title="My presentation">
 						<span>Slides</span>
 					</a>
 				</li>
