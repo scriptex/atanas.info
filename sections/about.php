5bc90ff82790e4c46aca2488d979c7a53de44140
Atanas Atanasov <scriptex.bg@gmail.com>
2019-05-03 09:07:46 +0300

    Update about section

--- sections/about.php
+++ sections/about.php
@@ -10,37 +10,29 @@
 
 				<p>I am currently located in Varna, Bulgaria. That's on the south-east side of Europe, on the Black Sea.</p>
 
-				<p>Many friends refer to me as "uncle" because I was born in the previous millennium but I am actually not that old. <br>Being born in the 1980s has its pros and cons.</p>
+				<p>I started my web development career in 2010 as an apprentice to a hardcore freelancer and pretty fast learned the basics of HTML and CSS.</p>
 
-				<p>The main pro about it is the fact that I've had enough time to decide what matters to me.</p>
+				<p>We had our own small coding company - <a href="http://html-lab.com/" target="_blank" rel="noopener noreferrer">HTML Lab</a>. In this company I was doing the front-end part and was mainly focused on slicing design files and turning them into static HTML pages.</p>
 			</div><!-- /.c-article__block -->
 
 			<div class="c-article__block">
-				<h3>I love coding!</h3>
+				<h3>First real job</h3>
 
-				<p>I started doing this in 2010 as an apprentice to a hardcore freelancer and pretty fast learned the basics of HTML and CSS.</p>
+				<p>Between 2012 and 2018 I was full-time employed in one of the larges companies in Varna, Bulgaria - 2create. In this company I worked for some of the biggest PSD to HTML brands out there - htmlBurger and htmlBoutique. I developed thousands (yes thousands) of different projects for clients from all over the globe and was focused on the front-end part of all projects.</p>
 
-				<p>We even had our own small coding "company" - <a href="http://html-lab.com/" target="_blank" rel="noopener noreferrer">HTML Lab</a>. In this company I participated as a frontend developer and was mainly focused on slicing design files and turning them into static HTML pages.</p>
+				<p>I participated in defining, integrating and teaching the internal coding standards in the company and was a mentor to several front-end developers.</p>
 			</div><!-- /.c-article__block -->
 
 			<div class="c-article__block">
-				<h3>It went serious:</h3>
+				<h3>JavaScript</h3>
 
-				<p>Between 2012 and 2018 I was full-time employed in one of the larges companies in Varna - 2create. In this company I worked for some of the biggest PSD to HTML brands out there - htmlBurger and htmlBoutique. I worked on thousands (yes thousands) of different projects for clients from all over the globe and was focused on the frontend part of all projects.</p>
+				<p>During my employment between 2012 and 2018 I developed, integrated and maintained the internal build systems for the company. This included mastering CSS pre- and post-processors such as LESS, SASS/SCSS and PostCSS, task runners such as Gulp and Grunt, module bundlers such as Webpack and Rollup, and NodeJS which I used as a runtime.</p>
+
+				<p>Along these technologies I mastered JavaScript and it became my preferred coding language. I also presented a keynote on the features introduced in 2015 with the 6th version of ECMAScript (the JavaScript standard).</p>
 			</div><!-- /.c-article__block -->
 
 			<div class="c-article__block">
-				<h3>I've grown up.</h3>
-
-				<p>In 2create I worked as a frontend developer and my daily tasks included slicing of design files (mainly PhotoShop, but also on Sketch, Figma, Adobe XD, etc.) and converting them into a pixel-perfect HTML and CSS pages.</p>
-
-				<p>As months went by, I started using pre- and post-processors in CSS and slowly mastered LESS, SASS and POSTCSS which opened the way for learning how to use task runners such as Grunt, Gulp and Webpack.</p>
-			</div><!-- /.c-article__block -->
-
-			<div class="c-article__block">
-				<h3>Enter JavaScript!</h3>
-
-				<p>Along CSS and all build tools, I've learned and mastered JavaScript and it became my preferred language. <br>I love it and it appears it shares the feelings.</p>
+				<h3>Progress</h3>
 
 				<p>Since the beginning of 2018 and until early 2019 I worked as a Senior JavaScript developer in a small startup located in Varna and operating internationally.</p>
 
@@ -50,7 +42,7 @@
 			<div class="c-article__block">
 				<h2>Today</h2>
 
-				<p>Now I am a JavaScript and TypeScript developer with more than 8 years of professional experience. I am focused on the frontend but using a lot of back-end technologies as well. <br>My daily tasks include coding HTML, CSS, JavaScript, TypeScript, PHP, Bash...</p>
+				<p>Now I am a JavaScript and TypeScript developer with more than 8 years of professional experience. I am focused on the front-end but using a lot of back-end technologies as well. <br>My daily tasks include coding HTML, CSS, JavaScript, TypeScript, PHP, Bash...</p>
 
 				<p>The latest project I worked on is a large international IoT application for one of the world's largest investor-owned electric utility service providers which aims to provide users from all over Europe with the ability to control their homes' smart appliances and energy consumption.</p>
 			</div><!-- /.c-article__block -->
