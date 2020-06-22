46f8091325c5530d077645ab2a3e68f2594301d1
Atanas Atanasov <scriptex.bg@gmail.com>
2020-06-22 16:32:28 +0300

    WIP: Add a music player component

--- src/components/section-music/index.tsx
+++ src/components/section-music/index.tsx
@@ -1,30 +1,18 @@
 import * as React from 'react';
-import Webamp from 'webamp';
+import Player from 'react-jinke-music-player';
 
 import { tracks } from '../../assets/scripts/tracks';
 
-export const SectionMusic: React.FunctionComponent = () => {
-	React.useEffect(() => {
-		const root = document.getElementById('root');
-		const music = document.getElementById('music');
-		const musicLink = document.querySelector('[href="#music"]');
-
-		if (Webamp.browserIsSupported() && music && root) {
-			const webamp = new Webamp({ initialTracks: tracks });
-
-			webamp.onClose(() => {
-				root.classList.remove('music--active');
-
-				if (musicLink) {
-					musicLink.addEventListener('click', () => webamp.reopen());
-				}
-			});
-
-			webamp.renderWhenReady(music);
-		}
-	});
-
-	return <div id="music"></div>;
-};
+export const SectionMusic: React.FunctionComponent = () => (
+	<Player
+		autoPlay={false}
+		audioLists={tracks.map(track => ({
+			name: track.metaData.title,
+			musicSrc: track.url,
+			singer: track.metaData.artist
+		}))}
+		defaultPosition={{ bottom: 'calc(50% - 30px)', left: 0 }}
+	/>
+);
 
 export default SectionMusic;
