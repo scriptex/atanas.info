607fc922085c88cc22bd3970fbe078f358cc90b9
Atanas Atanasov <scriptex.bg@gmail.com>
2020-07-02 17:12:04 +0300

    Add better music page

--- music/index.ts
+++ music/index.ts
@@ -1,40 +1,28 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
-import TweenLite from 'gsap';
 import SimplexNoise from 'simplex-noise';
+import { Power4, TweenMax } from 'gsap';
 
-const a: HTMLElement | null = document.getElementById('audio');
-const c: HTMLElement | null = document.getElementById('canvas');
+import { tracks } from '../src/scripts/tracks';
 
-const start: HTMLElement | null = document.querySelector('#start');
-const homepage: HTMLElement | null = document.querySelector('#homepage');
+const list = document.querySelector('#tracks') as HTMLDivElement;
+const menu = document.querySelector('#menu') as HTMLButtonElement;
+const audio = document.getElementById('audio') as HTMLMediaElement;
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-const playBtn: HTMLElement | null = document.querySelector('#overlay .time .btn.play');
-const pauseBtn: HTMLElement | null = document.querySelector('#overlay .time .btn.pause');
-const timeOverlay: HTMLElement | null = document.querySelector('#overlay .time');
-
-const audio = a as HTMLMediaElement;
-const canvas = c as HTMLCanvasElement;
-
-const dPI = Math.PI * 2;
-const fftSize = 2048;
-const nbParticles = 600;
+const playBtn = document.querySelector('#play') as HTMLButtonElement;
+const pauseBtn = document.querySelector('#pause') as HTMLButtonElement;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let safari = false;
+let WIDTH: number = canvas.width;
+let HEIGHT: number = canvas.height;
 
-let WIDTH = canvas.width;
-let HEIGHT = canvas.height;
-
-const ctx = canvas.getContext('2d');
-const order = [0, 1, 2];
-const simplex = new SimplexNoise();
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+const simplex: SimplexNoise = new SimplexNoise();
 const circles: Circle[] = [];
 
-let src;
-let context;
+let src: MediaElementAudioSourceNode;
+let context: AudioContext;
 
 let radius: number;
 let isPause = false;
@@ -42,85 +30,103 @@ let analyser: AnalyserNode;
 let dataArray: Uint8Array;
 let bufferLength: number;
 
-if (navigator.vendor.toLowerCase().includes('apple')) {
-	audio!.hidden = false;
-	document.body.classList.add('safari');
-	start!.innerText = '';
-	safari = true;
-}
+const createCircles = (): void => {
+	for (let i = 0; i < 600; ++i) {
+		circles.push(new Circle(i));
+	}
+};
 
-audio!.addEventListener('click', () => {
-	if (safari) {
-		TweenLite.to(homepage, 0.25, {
-			ease: Power4.easeIn,
-			opacity: 0,
-			onComplete() {
-				homepage!.style.display = 'none';
-			}
-		});
+const showCircles = (): void => {
+	for (const circle of circles) {
+		circle.update();
+		circle.draw();
+	}
+};
 
-		audio.volume = 0.5;
+const update = (): void => {
+	if (isPause) {
+		return;
+	}
 
-		context = new (window.AudioContext || (window as any).webkitAudioContext)();
+	radius = circles[3].value;
 
+	analyser.getByteFrequencyData(dataArray);
+
+	background('rgba(0,0,0,0.5)');
+	showCircles();
+	requestAnimationFrame(update);
+};
+
+const background = (color: string): void => {
+	ctx.fillStyle = color;
+	ctx.fillRect(0, 0, WIDTH, HEIGHT);
+};
+
+const drawCircle = (x: number, y: number, size: number, fill: string): void => {
+	ctx.save();
+	ctx.beginPath();
+	ctx.fillStyle = fill;
+	ctx.translate(x, y);
+	ctx.arc(0, 0, size, 0, Math.PI * 2);
+	ctx.fill();
+	ctx.closePath();
+	ctx.restore();
+};
+
+audio.src = tracks[0].url;
+audio.load();
+audio.volume = 0.5;
+
+list.innerHTML = tracks
+	.map(
+		track =>
+			`<button data-url="${track.url}"><em>Load</em><br /> <strong>${track.metaData.artist} - ${track.metaData.title}</strong></button>`
+	)
+	.join('\n');
+
+menu.addEventListener('click', (e: MouseEvent) => {
+	e.preventDefault();
+
+	list.hidden = false;
+});
+
+playBtn.addEventListener('click', async () => {
+	playBtn.hidden = true;
+	pauseBtn.hidden = false;
+
+	await audio.play();
+
+	context = new AudioContext();
+
+	if (!src) {
 		src = context.createMediaElementSource(audio);
+
 		analyser = context.createAnalyser();
+
 		src.connect(analyser);
+
 		analyser.connect(context.destination);
-		analyser.fftSize = fftSize;
+		analyser.fftSize = 2048;
 		analyser.smoothingTimeConstant = 0.5;
 		analyser.maxDecibels = -10;
+
 		bufferLength = analyser.frequencyBinCount;
+
 		dataArray = new Uint8Array(bufferLength);
-		isPause = false;
-		update();
-
-		timeOverlay!.classList.remove('pause');
-		timeOverlay!.classList.add('play');
 	}
-});
 
-start!.addEventListener('click', function () {
-	TweenLite.to(homepage, 0.25, {
-		ease: Power4.easeIn,
-		opacity: 0,
-		onComplete() {
-			homepage!.style.display = 'none';
-		}
-	});
-
-	audio.load();
-	audio.volume = 0.5;
-	context = new AudioContext();
-	src = context.createMediaElementSource(audio);
-	analyser = context.createAnalyser();
-	src.connect(analyser);
-	analyser.connect(context.destination);
-	analyser.fftSize = fftSize;
-	analyser.smoothingTimeConstant = 0.5;
-	analyser.maxDecibels = -10;
-	bufferLength = analyser.frequencyBinCount;
-	dataArray = new Uint8Array(bufferLength);
 	isPause = false;
-	update();
 
-	timeOverlay!.classList.remove('pause');
-	timeOverlay!.classList.add('play');
+	update();
 });
 
-pauseBtn!.addEventListener('click', function () {
+pauseBtn.addEventListener('click', () => {
+	playBtn.hidden = false;
+	pauseBtn.hidden = true;
+
 	audio.pause();
-	isPause = true;
-	timeOverlay!.classList.remove('play');
-	timeOverlay!.classList.add('pause');
-});
 
-playBtn!.addEventListener('click', function () {
-	audio.play();
-	isPause = false;
-	update();
-	timeOverlay!.classList.remove('pause');
-	timeOverlay!.classList.add('play');
+	isPause = true;
 });
 
 window.addEventListener('resize', () => {
@@ -133,9 +139,22 @@ window.addEventListener('resize', () => {
 	background('#000');
 });
 
-background('#000');
+Array.from(list.querySelectorAll('button')).forEach((button: HTMLButtonElement) => {
+	button.addEventListener('click', () => {
+		list.hidden = true;
+		playBtn.hidden = false;
+		pauseBtn.hidden = true;
 
-createCircles();
+		audio.pause();
+
+		audio.src = button.dataset.url || tracks[0].url;
+		audio.load();
+
+		isPause = true;
+	});
+});
+
+background('#000');
 
 class Circle {
 	private i: number;
@@ -190,8 +209,8 @@ class Circle {
 	draw() {
 		if (this.value !== this.cachedValue) {
 			this.rgbArray = [this.value + 25 * (this.index / bufferLength), 500 * (this.index / bufferLength), 50];
-			this.rgb = `rgb(${Math.round(this.rgbArray[order[0]])},${Math.round(this.rgbArray[order[1]])},${Math.round(
-				this.rgbArray[order[2]]
+			this.rgb = `rgb(${Math.round(this.rgbArray[0])},${Math.round(this.rgbArray[1])},${Math.round(
+				this.rgbArray[2]
 			)})`;
 		}
 
@@ -199,43 +218,4 @@ class Circle {
 	}
 }
 
-function createCircles() {
-	for (let i = 0; i < nbParticles; ++i) {
-		circles.push(new Circle(i));
-	}
-}
-
-function showCircles() {
-	for (let i = 0; i < circles.length; ++i) {
-		circles[i].update();
-		circles[i].draw();
-	}
-}
-
-function update(): void {
-	if (isPause) {
-		return;
-	}
-
-	analyser.getByteFrequencyData(dataArray);
-	background('rgba(0,0,0,0.5)');
-	radius = circles[3].value;
-	showCircles();
-	requestAnimationFrame(update);
-}
-
-function background(color: string): void {
-	ctx!.fillStyle = color;
-	ctx!.fillRect(0, 0, WIDTH, HEIGHT);
-}
-
-function drawCircle(x: number, y: number, size: number, fill: string): void {
-	ctx!.save();
-	ctx!.beginPath();
-	ctx!.fillStyle = fill;
-	ctx!.translate(x, y);
-	ctx!.arc(0, 0, size, 0, dPI);
-	ctx!.fill();
-	ctx!.closePath();
-	ctx!.restore();
-}
+createCircles();
