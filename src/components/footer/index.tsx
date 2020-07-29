81d3274e1f879bff277c97e8bbb555e3fd5ccd68
Atanas Atanasov <scriptex.bg@gmail.com>
2020-07-29 14:57:25 +0300

    Update socials

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-
-import { socials, Social } from '../../scripts/socials';
-import { ExternalLink, Icon } from '..';
+import 'scriptex-socials';
 
 export const Footer: React.FunctionComponent = () => (
 	<footer className="c-footer">
@@ -11,28 +9,7 @@ export const Footer: React.FunctionComponent = () => (
 				All rights reserved.
 			</p>
 
-			<nav className="c-socials">
-				<ul>
-					{socials.map((social: Social, index: number) => (
-						<li key={index}>
-							<ExternalLink href={social.url} title={social.title}>
-								<Icon name={`svg-${social.icon}`} className={`c-svg-${social.icon}`}></Icon>
-							</ExternalLink>
-						</li>
-					))}
-
-					<li>
-						<ExternalLink href="https://sourcerer.io/scriptex" title="See my profile on Sourcerer">
-							<img
-								src="https://sourcerer.io/icons/logo-sharing.svg"
-								alt="Sourcerer brand image"
-								width="32"
-								loading="lazy"
-							/>
-						</ExternalLink>
-					</li>
-				</ul>
-			</nav>
+			<social-links></social-links>
 		</div>
 	</footer>
 );
