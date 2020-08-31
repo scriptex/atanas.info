2925ed68e7f594bb2189eaddf2d3e8859b98115c
Atanas Atanasov <scriptex.bg@gmail.com>
2020-08-31 14:47:54 +0300

    Add two Houdini features

---
+++ src/houdini/slanted-backgrounds.ts
@@ -0,0 +1,41 @@
+registerPaint(
+	'slanted-background',
+	class {
+		static get inputProperties(): string[] {
+			return ['--slanted-background-color', '--slanted-background-opacity'];
+		}
+
+		static get contextOptions(): Record<string, boolean> {
+			return {
+				alpha: true
+			};
+		}
+
+		paint(
+			ctx: CanvasRenderingContext2D,
+			{ width, height }: Record<string, number>,
+			properties: Map<string, any>
+		): void {
+			const color: string = properties.get('--slanted-background-color');
+			const opacity: number = properties.get('--slanted-background-opacity');
+
+			ctx.fillStyle = color;
+
+			ctx.beginPath();
+			ctx.moveTo(0, 0);
+			ctx.lineTo(width, 0);
+			ctx.lineTo(width - 20, height);
+			ctx.lineTo(0, height);
+			ctx.fill();
+
+			ctx.globalCompositeOperation = 'source-atop';
+
+			ctx.fillStyle = `rgba(0, 0, 0 , ${opacity})`;
+			ctx.beginPath();
+			ctx.moveTo(0, height);
+			ctx.lineTo(width, height - 8);
+			ctx.lineTo(width, height);
+			ctx.fill();
+		}
+	}
+);
