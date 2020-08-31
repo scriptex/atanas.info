81dcd4afd99cb28770064266801e92fd8e15c016
Atanas Atanasov <scriptex.bg@gmail.com>
2020-08-31 10:40:50 +0300

    Update contact component

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -9,6 +9,58 @@ enum Status {
 	DEFAULT = 'DEFAULT'
 }
 
+export const ContactSuccessContent: React.FunctionComponent = () => (
+	<>
+		<h4>I am currently not available for hire but you can still</h4>
+
+		<h2>Send me your message</h2>
+
+		<div className="c-contact__field">
+			<label htmlFor="email">Email:</label>
+
+			<input id="email" type="email" name="email" required={true} />
+		</div>
+
+		<div className="c-contact__field">
+			<label htmlFor="message">Message:</label>
+
+			<textarea name="message" id="message" cols={30} rows={10} required={true}></textarea>
+		</div>
+	</>
+);
+
+export const onSubmit = (
+	e: React.FormEvent<HTMLFormElement>,
+	setStatus: (status: Status) => void,
+	setErrorMessage: (message: string) => void
+): void => {
+	e.preventDefault();
+
+	const form = e.target as HTMLFormElement;
+	const data = new FormData(form);
+
+	fetch(form.action, {
+		method: form.method,
+		headers: {
+			Accept: 'application/json'
+		},
+		body: data
+	})
+		.then((r: Response) => r.json())
+		.then((result: any) => {
+			if (result.error) {
+				setStatus(Status.ERROR);
+				setErrorMessage(result.error);
+			} else {
+				setStatus(Status.SUCCESS);
+			}
+		})
+		.catch(() => {
+			setStatus(Status.ERROR);
+			setErrorMessage('Something went wrong. Please try again.');
+		});
+};
+
 export const Contact: React.FunctionComponent = () => {
 	const [status, setStatus] = React.useState(Status.DEFAULT);
 	const [errorMessage, setErrorMessage] = React.useState('');
@@ -19,33 +71,7 @@ export const Contact: React.FunctionComponent = () => {
 			action="https://formspree.io/meqrwbvd"
 			method="POST"
 			className={`c-contact${contactVisible ? ' c-contact--visible' : ''}`}
-			onSubmit={(e: any) => {
-				e.preventDefault();
-
-				const form: HTMLFormElement = e.target;
-				const data = new FormData(form);
-
-				fetch(form.action, {
-					method: form.method,
-					headers: {
-						Accept: 'application/json'
-					},
-					body: data
-				})
-					.then((r: Response) => r.json())
-					.then((result: any) => {
-						if (result.error) {
-							setStatus(Status.ERROR);
-							setErrorMessage(result.error);
-						} else {
-							setStatus(Status.SUCCESS);
-						}
-					})
-					.catch(() => {
-						setStatus(Status.ERROR);
-						setErrorMessage('Something went wrong. Please try again.');
-					});
-			}}
+			onSubmit={(e: React.FormEvent<HTMLFormElement>) => onSubmit(e, setStatus, setErrorMessage)}
 		>
 			<button
 				type="button"
@@ -60,25 +86,7 @@ export const Contact: React.FunctionComponent = () => {
 			</button>
 
 			<div className="c-contact__body">
-				{status === Status.SUCCESS ? null : (
-					<>
-						<h4>I am currently not available for hire but you can still</h4>
-
-						<h2>Send me your message</h2>
-
-						<div className="c-contact__field">
-							<label htmlFor="email">Email:</label>
-
-							<input id="email" type="email" name="email" required={true} />
-						</div>
-
-						<div className="c-contact__field">
-							<label htmlFor="message">Message:</label>
-
-							<textarea name="message" id="message" cols={30} rows={10} required={true}></textarea>
-						</div>
-					</>
-				)}
+				{status === Status.SUCCESS ? null : <ContactSuccessContent />}
 
 				<div className="c-contact__actions">
 					{status === Status.SUCCESS ? (
