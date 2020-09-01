7df99119d9b2bde39946f55d9bb241ea0252489f
Atanas Atanasov <scriptex.bg@gmail.com>
2020-09-01 10:40:51 +0300

    Fix shell script

--- bin/clone.sh
+++ bin/clone.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-current_dir=`cd "$(dirname "$0")" && pwd`
+current_dir=$(cd "$(dirname "$0")" && pwd)
 
 cd ~/server/projects/open-source
 
-echo $current_dir
-
-while IFS=\= read repository; do
+while IFS== read -r repository; do
     echo "clonning $repository"
 	git clone $repository
 done < $current_dir/github.list
 
-while IFS=\= read repository; do
+while IFS== read -r repository; do
     echo "clonning $repository"
 	git clone $repository
 done < $current_dir/gitlab.list
