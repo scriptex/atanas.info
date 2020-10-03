d38fbaa2c03f52f00212071b221557931dd76e7a
Atanas Atanasov <scriptex.bg@gmail.com>
2020-10-03 11:40:43 +0300

    Rearrange sections

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,7 +10,6 @@ export { default as Section } from './section';
 export { default as SectionAbout } from './section-about';
 export { default as SectionCertificates } from './section-certificates';
 export { default as SectionHello } from './section-hello';
-export { default as SectionMusic } from './section-music';
 export { default as SectionNav } from './section-nav';
 export { default as SectionPortfolio } from './section-portfolio';
 export { default as SectionSkills } from './section-skills';
