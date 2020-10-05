2b4973de5b16a65982e3b390e0ab1253761767a6
Atanas Atanasov <scriptex.bg@gmail.com>
2020-10-05 09:34:15 +0300

    Add correct section classname

--- src/components/section-articles/index.tsx
+++ src/components/section-articles/index.tsx
@@ -4,7 +4,7 @@ import { articles, Article } from '../../scripts/articles';
 import { Section, ExternalLink } from '..';
 
 export const SectionArticles: React.FunctionComponent = () => (
-	<Section id="articles" hasButton={true} className=" c-section--articles">
+	<Section id="articles" hasButton={true} className=" c-section--slides">
 		<h2>Articles</h2>
 
 		<div className="c-section__body o-grid">
