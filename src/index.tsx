99c33180fc669e7e811aa8c0ce12902d33312c76
Atanas Atanasov <scriptex.bg@gmail.com>
2020-10-05 09:45:45 +0300

    Add offline support

--- src/index.tsx
+++ src/index.tsx
@@ -23,8 +23,34 @@ gtag('config', 'UA-83446952-1');
 
 if ('serviceWorker' in navigator) {
 	const sw = `service-worker.js`;
+	const ow = `offline-worker.js`;
 
 	window.addEventListener('load', () => {
 		navigator.serviceWorker.register(sw);
 	});
+
+	navigator.serviceWorker
+		.register(ow)
+		.then((registration: ServiceWorkerRegistration) => {
+			registration.onupdatefound = () => {
+				const installingWorker = registration.installing;
+
+				if (installingWorker === null) {
+					return;
+				}
+
+				installingWorker.onstatechange = () => {
+					if (installingWorker.state === 'installed') {
+						console.info(
+							navigator.serviceWorker.controller
+								? 'New content is available and will be used when all tabs for this page are closed.'
+								: 'Content is cached for offline use.'
+						);
+					}
+				};
+			};
+		})
+		.catch(error => {
+			console.error('Error during service worker registration: ', error);
+		});
 }
