339611a4db68cf0834d91464e92f2abcca9b829e
Atanas Atanasov <scriptex.bg@gmail.com>
2020-10-21 09:29:03 +0300

    Update tests

--- src/components/section-slides/index.test.tsx
+++ src/components/section-slides/index.test.tsx
@@ -1,22 +1,12 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 import * as React from 'react';
-import { act } from 'react-dom/test-utils';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { SectionSlides } from '.';
 
 describe('SectionSlides component', () => {
 	it('Should render the SectionSlides component', () => {
-		act(() => {
-			const wrapper = mount(<SectionSlides />);
+		const wrapper = shallow(<SectionSlides />);
 
-			expect(wrapper).toMatchSnapshot();
-
-			// @ts-ignore
-			wrapper.find('button').at(0).props().onClick();
-
-			expect(wrapper).toMatchSnapshot();
-		});
+		expect(wrapper).toMatchSnapshot();
 	});
 });
--- src/components/section-videos/index.test.tsx
+++ src/components/section-videos/index.test.tsx
@@ -1,22 +1,12 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-
 import * as React from 'react';
-import { act } from 'react-dom/test-utils';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { SectionVideos } from '.';
 
 describe('SectionVideos component', () => {
 	it('Should render the SectionVideos component', () => {
-		act(() => {
-			const wrapper = mount(<SectionVideos />);
+		const wrapper = shallow(<SectionVideos />);
 
-			expect(wrapper).toMatchSnapshot();
-
-			// @ts-ignore
-			wrapper.find('button').at(0).props().onClick();
-
-			expect(wrapper).toMatchSnapshot();
-		});
+		expect(wrapper).toMatchSnapshot();
 	});
 });
