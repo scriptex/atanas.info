72b8f223eb2970e8dad64f11beb20a9565200299
Atanas Atanasov <scriptex.bg@gmail.com>
2020-11-19 10:17:55 +0200

    Update dependencies

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	plugins: {
-		'postcss-for': true,
 		'postcss-preset-env': {
 			stage: 1,
 			features: {
@@ -11,9 +10,15 @@ module.exports = {
 		},
 		autoprefixer: true,
 		'postcss-import': true,
+		'postcss-each-variables': true,
+		'postcss-each': true,
+		'postcss-for': true,
 		'postcss-mixins': true,
+		'postcss-extend': true,
+		'postcss-utilities': true,
 		'postcss-flexbugs-fixes': true,
 		'postcss-merge-rules': true,
-		'postcss-custom-media': true
+		'postcss-custom-media': true,
+		'postcss-calc': true
 	}
 };
--- src/components/section-skills/index.tsx
+++ src/components/section-skills/index.tsx
@@ -28,7 +28,7 @@ export const SectionSkills: React.FunctionComponent = () => {
 
 			<div className={`o-grid${showTable ? '' : ' is--hidden'}`}>
 				{skills.map((skill: Skill, index: number) => (
-					<div key={index} className="o-grid__item o-grid__item--1of4">
+					<div key={index} className="o-grid__item xs-6 sm-4 md-3">
 						<div className="c-skill">
 							<figure style={{ backgroundColor: skill.fill }}>
 								<Icon
