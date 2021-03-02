510796c33fb57e3e2a3c6e30c7ea608bb1d36da0
Atanas Atanasov <scriptex.bg@gmail.com>
2021-03-02 11:34:06 +0200

    Increase workbox file size

--- workbox-config.js
+++ workbox-config.js
@@ -2,8 +2,9 @@ module.exports = {
 	swDest: 'dist/offline-worker.js',
 	sourcemap: false,
 	skipWaiting: true,
-	globPatterns: ['**/*.{js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'],
 	globIgnores: ['**/images/svg/*'],
+	globPatterns: ['**/*.{js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'],
 	clientsClaim: true,
-	globDirectory: 'dist'
+	globDirectory: 'dist',
+	maximumFileSizeToCacheInBytes: 8000000
 };
