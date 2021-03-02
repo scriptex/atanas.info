0049c5d8f55911569360ff01e1052c65128ae730
Atanas Atanasov <scriptex.bg@gmail.com>
2021-03-02 11:28:10 +0200

    Remove SVGs from workbox cache

--- workbox-config.js
+++ workbox-config.js
@@ -3,6 +3,7 @@ module.exports = {
 	sourcemap: false,
 	skipWaiting: true,
 	globPatterns: ['**/*.{js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}'],
+	globIgnores: ['**/images/svg/*'],
 	clientsClaim: true,
 	globDirectory: 'dist'
 };
