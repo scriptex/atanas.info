a06d3122f04336f57297a3b524ec6084663763c8
Atanas Atanasov <scriptex.bg@gmail.com>
2021-03-25 08:29:58 +0200

    Update funding widget

--- src/components/funding/index.tsx
+++ src/components/funding/index.tsx
@@ -13,37 +13,31 @@ interface Props {
 }
 
 export const FundingItem: React.FC<Props> = (props: Props) => (
-	// @ts-ignore
-	<a
-		rel="noopener noreferrer"
-		target="_blank"
-		className="c-funding__item"
-		// @ts-ignore
-		transform={props.matrix}
-		xlinkHref={props.url}
-	>
+	<g transform={props.matrix} className="c-funding__item">
 		<path fill="none" d="M250,250 l250,0 A250,250 0 0,0 375,33.49364905389035 z" />
 
-		<text
-			x="398"
-			y="200"
-			fill="var(--color-secondary)"
-			fontSize="1em"
-			textAnchor="middle"
-			transform="rotate(60 397.2243347167969 165)"
-		>
-			{props.name}
-		</text>
+		<a rel="noopener noreferrer" target="_blank" href={props.url}>
+			<text
+				x="398"
+				y="200"
+				fill="var(--color-secondary)"
+				fontSize="1em"
+				textAnchor="middle"
+				transform="rotate(60 397.2243347167969 165)"
+			>
+				{props.name}
+			</text>
 
-		<use
-			x="377.2243347167969"
-			y="145"
-			width="40"
-			height="40"
-			xlinkHref={`#svg-${props.icon}`}
-			transform="rotate(60 397.2243347167969 165)"
-		/>
-	</a>
+			<use
+				x="377.2243347167969"
+				y="145"
+				width="40"
+				height="40"
+				xlinkHref={`#svg-${props.icon}`}
+				transform="rotate(60 397.2243347167969 165)"
+			/>
+		</a>
+	</g>
 );
 
 export const Funding: React.FC = () => {
