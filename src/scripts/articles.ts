8572d6e751bec94c6478cd8ed044877b712dafb6
Atanas Atanasov <scriptex.bg@gmail.com>
2021-04-02 07:39:10 +0300

    Add articles from personal blog

--- src/scripts/articles.ts
+++ src/scripts/articles.ts
@@ -14,5 +14,15 @@ export const articles: Article[] = [
 		url: 'https://htmlboutique.com/blog/2015/11/13/ionic-development-by-htmlboutique/',
 		title: 'Ionic Development by htmlBoutique',
 		image: 'https://htmlboutique.com/blog/wp-content/uploads/2015/11/ionicnew3.jpg'
+	},
+        {
+		url: 'https://scriptexbg.wordpress.com/2021/03/11/bulk-image-watermark-with-nodejs-and-jimp/',
+		title: 'Bulk image watermark with NodeJS and Jimp',
+		image: 'https://scriptexbg.files.wordpress.com/2021/03/3phjrjs.jpg'
+	},
+	{
+		url: 'https://scriptexbg.wordpress.com/2021/02/19/running-multiple-webpack-based-applications-simultaneously/',
+		title: 'Running multiple Webpack based applications simultaneously',
+		image: 'https://scriptexbg.files.wordpress.com/2021/02/nodejs-1.png'
 	}
 ];
