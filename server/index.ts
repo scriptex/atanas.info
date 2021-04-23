9f6821bcf1f813f80f98917ef0418faedfc0a9d8
Atanas Atanasov <scriptex.bg@gmail.com>
2021-04-23 15:46:28 +0300

    Update imports

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,18 @@
 import * as globby from 'globby';
 
 import { resolve } from 'path';
-import { promises } from 'fs';
-
-const { readFile, writeFile } = promises;
+import { promises as fs } from 'fs';
 
 const readFiles = async (dirname: string, onSuccess: (filename: string, content: string) => unknown) => {
 	const files = await globby(`${dirname}/**/*.html`);
 
 	for (const file of files) {
-		const content = await readFile(resolve(dirname, file));
+		const content = await fs.readFile(resolve(dirname, file));
 
 		await onSuccess(file, content.toString());
 	}
 };
 
 readFiles(resolve(__dirname, '..', 'dist'), async (filename: string, content: string) => {
-	await writeFile(filename, content.replace(/http:\/\/localhost:45678\//gim, '/'));
+	await fs.writeFile(filename, content.replace(/http:\/\/localhost:45678\//gim, '/'));
 });
