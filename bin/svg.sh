0f72d7782cd84df4bc92382ce873ab5703918e80
Atanas Atanasov <scriptex.bg@gmail.com>
2021-12-13 13:38:40 +0200

    Fix SVG sprite on Linux

--- bin/svg.sh
+++ bin/svg.sh
@@ -6,3 +6,12 @@ svgo-viewbox -i static/images/svg
 spritesh -q -i static/images/svg -o static/sprite.svg -p svg-
 
 html-minifier static/sprite.svg --collapse-whitespace -o static/sprite.svg
+
+# Remove SVG sprite's `visibility: hidden; ` style because it breaks the icons on Linux 🤷‍♂️
+file=$PWD/static/sprite.svg
+string="visibility: hidden; "
+
+svg=$(<$file)
+svg=${svg//$string/}
+
+echo $svg > $file
