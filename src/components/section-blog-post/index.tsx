c26b245e2d8ed488d0eb58783a0c433149f71db0
Atanas Atanasov <scriptex.bg@gmail.com>
2021-12-14 17:21:04 +0200

    Use lighter react-syntax-highlighter (#1644)

--- src/components/section-blog-post/index.tsx
+++ src/components/section-blog-post/index.tsx
@@ -4,10 +4,27 @@ import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { match } from 'react-router-dom';
 // @ts-ignore
 import { MDXProvider } from '@mdx-js/react';
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+
+import js from 'react-syntax-highlighter/dist/esm/languages/hljs/javascript';
+import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import css from 'react-syntax-highlighter/dist/esm/languages/hljs/css';
+import scss from 'react-syntax-highlighter/dist/esm/languages/hljs/scss';
+import json from 'react-syntax-highlighter/dist/esm/languages/hljs/json';
+import shell from 'react-syntax-highlighter/dist/esm/languages/hljs/shell';
 
 import { articles } from '../../data/articles';
 import { Lines, Section } from '..';
+
+SyntaxHighlighter.registerLanguage('js', js);
+SyntaxHighlighter.registerLanguage('ts', ts);
+SyntaxHighlighter.registerLanguage('sh', shell);
+SyntaxHighlighter.registerLanguage('css', css);
+SyntaxHighlighter.registerLanguage('scss', scss);
+SyntaxHighlighter.registerLanguage('json', json);
+SyntaxHighlighter.registerLanguage('shell', shell);
+SyntaxHighlighter.registerLanguage('javascript', js);
+SyntaxHighlighter.registerLanguage('typescript', ts);
 
 interface Params {
 	slug: string;
