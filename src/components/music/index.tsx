b9076ec6aa351dee14d3f6f52b9eede7d39bbf87
Atanas Atanasov <scriptex.bg@gmail.com>
2022-08-22 13:33:53 +0300

    Update music component

--- src/components/music/index.tsx
+++ src/components/music/index.tsx
@@ -1,43 +1,86 @@
 import * as React from 'react';
 
 import { music } from '../../scripts/music';
+import { tracks } from '../../data/tracks';
 import { Section } from '..';
 
 export const Music: React.FC = () => {
+	const [source, setSource] = React.useState(tracks[0]);
+	const [playing, setPlaying] = React.useState(false);
+	const [visible, setVisible] = React.useState(false);
+	const [functions, setFunctions] = React.useState<any>({});
+
+	const audio: React.MutableRefObject<HTMLAudioElement | null> = React.useRef(null);
+	const canvas: React.MutableRefObject<HTMLCanvasElement | null> = React.useRef(null);
 	const container: React.MutableRefObject<HTMLDivElement | null> = React.useRef(null);
 
 	React.useEffect(() => {
-		music(container.current);
+		setFunctions(music(container.current));
 	}, []);
 
 	return (
 		<Section id="music" hasButton wrapperClassName="o-main--high">
 			<div ref={container} className="c-music">
-				<canvas id="canvas" />
+				<audio ref={audio} src={source.url} hidden controls crossOrigin="anonymous" />
 
-				<button id="menu" className="c-music__menu">
+				<canvas ref={canvas} style={{ opacity: playing ? 1 : 0 }} />
+
+				<button className="c-music__menu" onClick={() => setVisible(true)}>
 					Pick a track
 				</button>
 
-				<button id="pause" className="c-music__btn c-music__btn--pause" hidden>
+				<button
+					hidden={!playing}
+					onClick={() => {
+						setPlaying(false);
+						setVisible(false);
+						audio.current?.pause();
+						functions?.onPause();
+					}}
+					className="c-music__btn c-music__btn--pause"
+				>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
 					</svg>
 					Pause
 				</button>
 
-				<button id="play" className="c-music__btn c-music__btn--play">
+				<button
+					hidden={playing}
+					onClick={() => {
+						setPlaying(true);
+						setVisible(false);
+						functions?.onPlay();
+						audio.current?.play();
+					}}
+					className="c-music__btn c-music__btn--play"
+				>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 						<path d="M8 5v14l11-7z" />
 					</svg>
 					Play
 				</button>
 
-				<audio id="audio" hidden controls crossOrigin="anonymous" />
+				<div className="c-music__tracks" hidden={!visible}>
+					{tracks.map((track, index) => (
+						<button
+							key={index}
+							onClick={async () => {
+								await setSource(track);
 
-				<div id="tracks" className="c-music__tracks" hidden />
+								setPlaying(true);
+								setVisible(false);
+								functions?.onPlay();
+								audio.current?.play();
+							}}
+							className={track.url === source.url ? 'active' : undefined}
+						>
+							{track.metaData.artist} - {track.metaData.title}
+						</button>
+					))}
+				</div>
 
-				<h2>
+				<h2 style={{ opacity: playing ? 0 : 1 }}>
 					Pick a track and <br /> press the play button.
 				</h2>
 			</div>
