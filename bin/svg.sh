54f490b8b6f7a827f81c24892e4eb1c5a0f634e0
Atanas Atanasov <hi@atanas.info>
2023-08-14 14:53:48 +0300

    Upgrade packages

--- bin/svg.sh
+++ bin/svg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-svgo-viewbox -i src/svg -f svgo.config.js
+svgo-viewbox -i src/svg -f svgo-config.mjs
 
-svg-symbol-sprite -i src/svg -o src/sprite.svg -p svg-
+svg-symbol-sprite -i src/svg -o src/sprite.svg -p svg- -c svgo-config.mjs
 
 html-minifier src/sprite.svg --collapse-whitespace -o src/sprite.svg
