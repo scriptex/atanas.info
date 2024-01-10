7deab61207e8cfec63bcf606608b7151f1c6a9c6
Atanas Atanasov <hi@atanas.info>
2024-01-10 15:16:12 +0200

    Fix metadata (#2692)

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,8 +1,6 @@
 import NextHead from 'next/head';
 import type { FC } from 'react';
 
-import { MetaTag, metaTags, LinkTag, linkTags } from '@data/meta';
-
 export const Head: FC = () => (
 	<NextHead>
 		<meta
@@ -10,13 +8,317 @@ export const Head: FC = () => (
 			content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
 		/>
 
-		{metaTags.map((data: MetaTag) => (
-			<meta {...data} key={data.index} />
-		))}
+		<meta
+			name="description"
+			content="Atanas Atanasov - Senior Javascript/Typescript Engineer: Single Page Applications, Vue, React, Angular, NodeJS | Professional web and mobile development services"
+		/>
 
-		{linkTags.map((data: LinkTag) => (
-			<link {...data} key={data.index} />
-		))}
+		<meta
+			name="keywords"
+			content="React Development, NodeJS Development, Typescript Development, Web Development, WordPress Development, HTML Development, Website Development, Javascript Development, WordPress, WP, JS, Javascript, Vue, Vue.js, VueJS, Nuxt, React, ReactJS, React JS, Node, NodeJS, Single Page Apps, Single Page Application, CSS, CSS3, ES5, ES6, ES7, Website, Development"
+		/>
+
+		<meta name="copyright" content="Atanas Atanasov" />
+
+		<meta name="language" content="EN" />
+
+		<meta name="robots" content="index, follow" />
+
+		<meta name="author" content="Atanas Atanasov, hi@atanas.info" />
+
+		<meta name="designer" content="Atanas Atanasov" />
+
+		<meta name="coverage" content="Worldwide" />
+
+		<meta name="distribution" content="Worldwide" />
+
+		<meta name="rating" content="General" />
+
+		<meta name="revisit-after" content="7 days" />
+
+		<meta content="0" httpEquiv="Expires" />
+
+		<meta content="no-cache" httpEquiv="Pragma" />
+
+		<meta content="no-cache" httpEquiv="Cache-Control" />
+
+		<meta content="https://atanas.info" property="og:url" />
+
+		<meta content="website" property="og:type" />
+
+		<meta content="Atanas Atanasov | Senior Javascript/Typescript Engineer" property="og:title" />
+
+		<meta content="https://atanas.info/images/temp/atanas.jpg" property="og:image" />
+
+		<meta
+			property="og:description"
+			content="Atanas Atanasov - Senior Javascript/Typescript Engineer: Single Page Applications, Vue, React, Angular, NodeJS | Professional web and mobile development services"
+		/>
+
+		<meta name="application-name" content="Atanas Atanasov" />
+
+		<meta name="mobile-web-app-capable" content="yes" />
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+
+		<meta name="apple-mobile-web-app-title" content="Atanas Atanasov" />
+
+		<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+
+		<meta name="format-detection" content="telephone=no" />
+
+		<meta name="msapplication-config" content="config.xml" />
+
+		<meta name="msapplication-TileColor" content="#ef4c23" />
+
+		<meta name="msapplication-TileImage" content="/images/favicon/ms-tile-144x144.png" />
+
+		<meta name="msapplication-square70x70logo" content="/images/favicon/ms-tile-70x70.png" />
+
+		<meta name="msapplication-square150x150logo" content="/images/favicon/ms-tile-150x150.png" />
+
+		<meta name="msapplication-wide310x150logo" content="/images/favicon/ms-tile-310x150.png" />
+
+		<meta name="msapplication-square310x310logo" content="/images/favicon/ms-tile-310x310.png" />
+
+		<meta name="theme-color" content="#ef4c23" />
+
+		<meta name="twitter:card" content="summary" />
+
+		<meta name="twitter:url" content="https://atanas.info" />
+
+		<meta name="twitter:title" content="Atanas Atanasov" />
+
+		<meta
+			name="twitter:description"
+			content="Atanas Atanasov - Senior Javascript/Typescript Engineer: Single Page Applications, Vue, React, Angular, NodeJS | Professional web and mobile development services"
+		/>
+
+		<meta name="twitter:image" content="https://atanas.info/images/temp/atanas.jpg" />
+
+		<meta name="twitter:creator" content="@scriptexbg" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-57x57.png" sizes="57x57" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-114x114.png" sizes="114x114" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-72x72.png" sizes="72x72" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-144x144.png" sizes="144x144" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-60x60.png" sizes="60x60" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-120x120.png" sizes="120x120" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-76x76.png" sizes="76x76" />
+
+		<link rel="apple-touch-icon" href="/images/favicon/apple-touch-icon-152x152.png" sizes="152x152" />
+
+		<link rel="icon" type="image/png" href="/images/favicon/favicon-196x196.png" sizes="196x196" />
+
+		<link rel="icon" type="image/png" href="/images/favicon/favicon-96x96.png" sizes="96x96" />
+
+		<link rel="icon" type="image/png" href="/images/favicon/favicon-32x32.png" sizes="32x32" />
+
+		<link rel="icon" type="image/png" href="/images/favicon/favicon-16x16.png" sizes="16x16" />
+
+		<link rel="icon" type="image/png" href="/images/favicon/favicon-128x128.png" sizes="128x128" />
+
+		<link rel="shortcut icon" type="image/x-icon" href="/images/favicon/favicon.ico" />
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2048x2732.png"
+			media="(device-width: 2048px) and (device-height: 2732 />and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2732x2048.png"
+			media="(device-width: 2732px) and (device-height: 2048px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1668x2388.png"
+			media="(device-width: 1668px) and (device-height: 2388px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2388x1668.png"
+			media="(device-width: 2388px) and (device-height: 1668px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1668x2224.png"
+			media="(device-width: 1668px) and (device-height: 2224px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2224x1668.png"
+			media="(device-width: 2224px) and (device-height: 1668px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1536x2048.png"
+			media="(device-width: 1536px) and (device-height: 2048px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2048x1536.png"
+			media="(device-width: 2048px) and (device-height: 1536px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1536x2048.png"
+			media="(device-width: 1536px) and (device-height: 2048px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2048x1536.png"
+			media="(device-width: 2048px and (device-height: 1536px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1242x2688.png"
+			media="(device-width: 1242px and (device-height: 2688px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2688x1242.png"
+			media="(device-width: 2688px) and (device-height: 142px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1125x2436.png"
+			media="(device-width: 1125px) and (device-height: 236px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2436x1125.png"
+			media="(device-width: 2436px) and (device-height: 1125px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-828x1792.png"
+			media="(device-width: 828px) and (device-height: 192px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1792x828.png"
+			media="(device-width: 1792px) and (device-height: 28px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1125x2436.png"
+			media="(device-width: 1125px) and (device-height: 236px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2436x1125.png"
+			media="(device-width: 2436px and (device-height: 1125px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1242x2208.png"
+			media="(device-width: 1242px and (device-height: 2208px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2208x1242.png"
+			media="(device-width: 2208px) and (device-height: 1242px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-750x1334.png"
+			media="(device-width: 750px) and (device-height: 134px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1334x750.png"
+			media="(device-width: 1334px and (device-height: 750px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1242x2208.png"
+			media="(device-width: 1242px and (device-height: 2208px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2208x1242.png"
+			media="(device-width: 2208px) and (device-height: 1242px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-750x1334.png"
+			media="(device-width: 750px) and (device-height: 134px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1334x750.png"
+			media="(device-width: 1334px and (device-height: 750px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1242x2208.png"
+			media="(device-width: 1242px and (device-height: 2208px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-2208x1242.png"
+			media="(device-width: 2208px) and (device-height: 1242px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-750x1334.png"
+			media="(device-width: 750px) and (device-height: 134px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1334x750.png"
+			media="(device-width: 1334px) and (device-height: 50px) and  (orientation: landscape)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-640x1136.png"
+			media="(device-width: 640px) and (device-height: 136px) and  (orientation: portrait)"
+		/>
+		
+		<link
+			rel="apple-touch-startup-image"
+			href="/images/launch-screens/launch-screen-1136x640.png"
+			media="(device-width: 1136px) and (device-height: 640px) and  (orientation: landscape)"
+		/>
+		
+		<link rel="manifest" href="/manifest.json" />
 	</NextHead>
 );
 
