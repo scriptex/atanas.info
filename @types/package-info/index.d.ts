39fc42dc4e4c856497e6b3aa9200de4e8b1c79cf
Atanas Atanasov <scriptex.bg@gmail.com>
2024-03-20 11:04:34 +0200

    Update package-info

--- @types/package-info/index.d.ts
+++ @types/package-info/index.d.ts
@@ -1,14 +1,10 @@
-declare module 'package-info' {
-	export type Package = {
-		author: string;
-		description: string;
-		homepage: string;
-		license: string;
-		name: string;
-		version: string;
-	};
-
-	export default function info(name: string): Promise<Package>;
-
-	export {};
+export interface Package {
+	author: string;
+	description: string;
+	homepage: string;
+	license: string;
+	name: string;
+	version: string;
 }
+export default function info(name: string): Promise<Package>;
+export {};
