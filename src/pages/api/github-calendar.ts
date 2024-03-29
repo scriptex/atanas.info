8dab8032c9b40f0fee1855beabc7c3cd0dff4320
Atanas Atanasov <scriptex.bg@gmail.com>
2024-03-29 09:09:16 +0200

    Use different puppeteer in CI

--- src/pages/api/github-calendar.ts
+++ src/pages/api/github-calendar.ts
@@ -1,8 +1,15 @@
+import chromium from '@sparticuz/chromium';
 import { JSDOM } from 'jsdom';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import * as puppeteer from 'puppeteer';
+import * as puppeteerOriginal from 'puppeteer';
+import * as puppeteerCore from 'puppeteer-core';
 
 import type { GithubProfileData } from '@scripts/types';
+
+chromium.setHeadlessMode = true;
+
+const isDev = process.env.NODE_ENV === 'development';
+const puppeteer = isDev ? puppeteerOriginal : puppeteerCore;
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse): Promise<void> {
 	const emptyResponse: GithubProfileData = {
@@ -12,8 +19,10 @@ export default async function handler(_: NextApiRequest, res: NextApiResponse): 
 
 	try {
 		const browser = await puppeteer.launch({
-			args: ['--no-sandbox'],
-			headless: true
+			args: [...(isDev ? puppeteer.defaultArgs() : chromium.args), '--no-sandbox'],
+			defaultViewport: chromium.defaultViewport,
+			executablePath: isDev ? puppeteer.executablePath() : await chromium.executablePath(),
+			headless: isDev ? false : chromium.headless
 		});
 
 		const page = await browser.newPage();
