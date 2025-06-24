df6179abdd1a5a322a44102a324b04ca67151bd7
Atanas Atanasov <scriptex.bg@gmail.com>
2025-06-24 18:45:52 +0300

    Update snapshots

--- tests/src/components/contact/index.tsx
+++ tests/src/components/contact/index.tsx
@@ -7,6 +7,9 @@ import { mockFetch, snapshotTest, test } from '@test-config/helpers';
 
 const fetch = global.fetch;
 const context = { contactVisible: true, setContactVisible: jest.fn() };
+const mockSubmit = jest.fn();
+
+jest.spyOn(HTMLFormElement.prototype, 'submit').mockImplementation(mockSubmit);
 
 afterAll(() => {
 	global.fetch = fetch;
@@ -35,13 +38,17 @@ it('Tests Contact submission', async () => {
 
 	mockFetch({ error: 'Something went wrong!' });
 
-	await act(async () => await form.submit()); // NOSONAR
+	await act(async () => form.submit());
+
+	expect(mockSubmit).toHaveBeenCalled();
 
 	expect(asFragment()).toMatchSnapshot();
 
 	mockFetch({ success: true });
 
-	await act(async () => await form.submit()); // NOSONAR
+	await act(async () => form.submit());
+
+	expect(mockSubmit).toHaveBeenCalled();
 
 	expect(asFragment()).toMatchSnapshot();
 });
